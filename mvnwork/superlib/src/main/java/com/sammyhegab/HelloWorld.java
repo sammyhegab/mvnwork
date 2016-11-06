f9222a8e69095253ec44d351c845ef84d6f774f0
Dell-PC\Admin <sammy.hegab@outlook.com>
2016-11-06 02:02:21 +0000

    new java super library project

---
+++ mvnwork/superlib/src/main/java/com/sammyhegab/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sammyhegab;
+
+/**
+ * Created by Admin on 05/11/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Me");
+    }
+}
